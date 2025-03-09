cd855f4a670458822b5a181922429850b6da4e01
yunsseong <aster20@hanyang.ac.kr>
2025-03-09 23:25:54 +0900

    feat: 단축 URL key 생성 기능 구현

--- src/main/java/yunsseong/shortenurl/service/ShortenUrlKeyGenerator.java
+++ src/main/java/yunsseong/shortenurl/service/ShortenUrlKeyGenerator.java
@@ -2,13 +2,23 @@ package yunsseong.shortenurl.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import yunsseong.shortenurl.limit.Limitable;
 
 @Service
 @RequiredArgsConstructor
 public class ShortenUrlKeyGenerator {
     private final String stringPool = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
     private final RandomNumberGenerator randomNumberGenerator;
+    private final Limitable shortenUrlKeyLengthLimit;
+
+    public String generateShortenUrlKey() {
+        StringBuilder shortenUrlKey = new StringBuilder();
+        do {
+            String value = generateRandomValue();
+            shortenUrlKey.append(value);
+        } while (shortenUrlKey.length() != shortenUrlKeyLengthLimit.getLimit());
+        return shortenUrlKey.toString();
+    }
 
     public String generateRandomValue() {
         int randomNum = randomNumberGenerator.generateRandomNumber(stringPool.length());
