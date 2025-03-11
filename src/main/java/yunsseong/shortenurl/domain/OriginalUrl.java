8baec3482f2c066ebe3287df2641c59eb33a8f6e
yunsseong <aster20@hanyang.ac.kr>
2025-03-11 16:06:57 +0900

    feat: 원본 URL 보관 및 접근 횟수 관리 클래스 구현

---
+++ src/main/java/yunsseong/shortenurl/domain/OriginalUrl.java
@@ -0,0 +1,23 @@
+package yunsseong.shortenurl.domain;
+
+public class OriginalUrl {
+    private String url;
+    private Long accessCount = 0L;
+
+    public OriginalUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        countUp();
+        return url;
+    }
+
+    public Long getAccessCount() {
+        return accessCount;
+    }
+
+    private void countUp() {
+        accessCount++;
+    }
+}
