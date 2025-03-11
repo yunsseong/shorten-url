79aca496e7a3a925cf7a3c54bf60af3b92ef6390
yunsseong <aster20@hanyang.ac.kr>
2025-03-11 15:32:44 +0900

    feat: 고유한 단축 URL 키 생성 기능 구현

--- src/main/java/yunsseong/shortenurl/service/ShortenUrlKeyGenerator.java
+++ src/main/java/yunsseong/shortenurl/service/ShortenUrlKeyGenerator.java
@@ -1,5 +1,7 @@
 package yunsseong.shortenurl.service;
 
+import java.util.HashSet;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import yunsseong.shortenurl.limit.Limitable;
@@ -7,9 +9,23 @@ import yunsseong.shortenurl.limit.Limitable;
 @Service
 @RequiredArgsConstructor
 public class ShortenUrlKeyGenerator {
-    private final String stringPool = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     private final RandomNumberGenerator randNumGen;
     private final Limitable limit;
+    private final Set<String> urlKeySet = new HashSet<>();
+    private long maxLimit = 0L;
+
+    public String getUniqueShortenUrlKey() {
+        if (urlKeySet.size() == maxLimit) {
+            throw new RuntimeException("키를 생성할 수 없습니다.");
+        }
+
+        String shortenUrlKey = "";
+        do {
+            shortenUrlKey = generateShortenUrlKey();
+        } while (urlKeySet.contains(shortenUrlKey));
+        urlKeySet.add(shortenUrlKey);
+        return shortenUrlKey;
+    }
 
     public String generateShortenUrlKey() {
         StringBuilder shortenUrlKey = new StringBuilder();
@@ -23,5 +39,12 @@ public class ShortenUrlKeyGenerator {
     public String generateRandomValue() {
         int randomNum = randNumGen.generateRandomNumber(stringPool.length());
         return String.valueOf(stringPool.charAt(randomNum));
+    }
+
+    public long getMaxLimit() {
+        if (maxLimit == 0) {
+            maxLimit = (long) Math.pow(stringPool.length(), limit.getLimit());
+        }
+        return maxLimit;
     }
 }
