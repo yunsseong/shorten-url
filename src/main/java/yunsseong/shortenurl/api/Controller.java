58f3c4735e77e5e13ae3211745256fe1a98dd7f0
yunsseong <aster20@hanyang.ac.kr>
2025-03-11 16:07:04 +0900

    feat: 단축 URL 생성 및 조회용 API 구현

---
+++ src/main/java/yunsseong/shortenurl/api/Controller.java
@@ -0,0 +1,30 @@
+package yunsseong.shortenurl.api;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import yunsseong.shortenurl.service.UrlMapper;
+
+@RestController
+@RequiredArgsConstructor
+public class Controller {
+
+    private final UrlMapper urlMapper;
+
+    @GetMapping("/{key}")
+    public ResponseEntity<String> getOriginalUrl(@PathVariable String key) {
+        return ResponseEntity.ok(urlMapper.getOriginalUrlByKey(key));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> registerUrl(@RequestBody String originalUrl, HttpServletRequest request) {
+        String mappedKey = urlMapper.makeNewMapping(originalUrl);
+        String shortenUrl = request.getRequestURL() + mappedKey;
+        return ResponseEntity.ok(shortenUrl);
+    }
+}
