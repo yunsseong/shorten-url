ba386f01a976d55c9a81f2a8f514a148ead98aea
yunsseong <aster20@hanyang.ac.kr>
2025-03-20 09:33:06 +0900

    feat: 전역 예외 처리 기능 구현

---
+++ src/main/java/yunsseong/shortenurl/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package yunsseong.shortenurl.common.exception;
+
+import java.time.LocalDateTime;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import yunsseong.shortenurl.common.exception.error_code.ErrorCode;
+import yunsseong.shortenurl.common.dto.response.ErrorResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
+        ErrorCode errorCode = ex.getErrorCode();
+        ErrorResponse errorResponse = new ErrorResponse(errorCode.getHttpStatus(), ex.getMessage(),
+                LocalDateTime.now());
+        return ResponseEntity.status(errorCode.getHttpStatus()).body(errorResponse);
+    }
+}
