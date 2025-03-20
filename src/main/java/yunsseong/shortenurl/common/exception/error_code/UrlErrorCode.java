4aef3c703a2c90e4f5379e812214b59bfc5cd158
yunsseong <aster20@hanyang.ac.kr>
2025-03-20 10:27:49 +0900

    feat: 잘못된 구조 URL 에러 추가

--- src/main/java/yunsseong/shortenurl/common/exception/error_code/UrlErrorCode.java
+++ src/main/java/yunsseong/shortenurl/common/exception/error_code/UrlErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum UrlErrorCode implements ErrorCode {
-    NOT_EXIST_URL(HttpStatus.BAD_REQUEST, "존재하지 않는 URL 입니다.");
+    NOT_EXIST_URL(HttpStatus.BAD_REQUEST, "존재하지 않는 URL 입니다."),
+    MALFORMED_URL(HttpStatus.BAD_REQUEST, "잘못된 구조의 URL 입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
