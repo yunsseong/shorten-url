fe0209a824b3d1190714e00ddfcdc4effa5abb8c
yunsseong <aster20@hanyang.ac.kr>
2025-03-20 09:36:33 +0900

    fix: 존재하지 않는 URL 조회 에러 메세지 수정

--- src/main/java/yunsseong/shortenurl/common/exception/error_code/UrlErrorCode.java
+++ src/main/java/yunsseong/shortenurl/common/exception/error_code/UrlErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum UrlErrorCode implements ErrorCode {
-    NOT_EXIST_URL(HttpStatus.BAD_REQUEST, "Url not exist");
+    NOT_EXIST_URL(HttpStatus.BAD_REQUEST, "존재하지 않는 URL 입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
