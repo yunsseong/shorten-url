dfefbc43d3a6b856c73fb0160a97aa51d03ece64
yunsseong <aster20@hanyang.ac.kr>
2025-03-20 09:32:43 +0900

    feat: 커스텀 예외 구현

---
+++ src/main/java/yunsseong/shortenurl/common/exception/CustomException.java
@@ -0,0 +1,16 @@
+package yunsseong.shortenurl.common.exception;
+
+import yunsseong.shortenurl.common.exception.error_code.ErrorCode;
+
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
