5c8d6ec4c24ebc942a2fb38a70a12fcbf43b9a66
yunsseong <aster20@hanyang.ac.kr>
2025-03-21 19:40:23 +0900

    feat: 연결할 수 없는 URL 에러 코드 추가

--- src/main/java/yunsseong/shortenurl/common/exception/error_code/UrlErrorCode.java
+++ src/main/java/yunsseong/shortenurl/common/exception/error_code/UrlErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UrlErrorCode implements ErrorCode {
     NOT_EXIST_URL(HttpStatus.BAD_REQUEST, "존재하지 않는 URL 입니다."),
-    MALFORMED_URL(HttpStatus.BAD_REQUEST, "잘못된 구조의 URL 입니다.");
+    MALFORMED_URL(HttpStatus.BAD_REQUEST, "잘못된 구조의 URL 입니다."),
+    UNREACHABLE_URL(HttpStatus.BAD_REQUEST, "연결할 수 없는 URL 입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
